18ac0bac2e14839d33315456b5a5d230168336e4
Vlad-Ryndin <99115954+Vlad-Ryndin@users.noreply.github.com>
2022-02-20 21:02:21 +0500

    Mission Complete!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 const swiper = new Swiper('.main-slider', {
-    slidesPerView: 2,
+
     spaceBetween: 30,
     loop: true,
     navigation: {
         nextEl: '.main-slider__arrow',
     },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+let burger = document.querySelector('.burger__menu');
+let closeMenu = document.querySelector('.header__menu-close')
+let menuMobile = document.querySelector('.header')
+
+burger.addEventListener('click', () => {
+    menuMobile.style.display = 'block';
+    burger.style.display = 'none';
+});
+
+closeMenu.addEventListener('click', () => {
+    menuMobile.style.display = 'none';
+    burger.style.display = 'block';
 });
